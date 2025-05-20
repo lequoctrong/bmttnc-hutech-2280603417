f49b123597555932ce967b64b56d16afec1dc704
lequoctrong <qctrong136@gmail.com>
2025-05-20 19:34:19 +0700

    [add] lab-02 web caesar

---
+++ lab-02/cipher/transposition/transposition_cipher.py
@@ -0,0 +1,26 @@
+class TranspositionCipher:
+    def __init__(self):
+        pass
+
+    def encrypt(self, text, key):
+        encrypted_text = ""
+        for col in range(key):
+            pointer = col
+            while pointer < len(text):
+                encrypted_text += text[pointer]
+                pointer += key
+        return encrypted_text
+    def decrypt(self, text, key):
+        decrypted_text = [''] * key  # Khởi tạo danh sách rỗng với số phần tử bằng key
+        row, col = 0, 0              # Khởi tạo hàng và cột
+
+        for symbol in text:
+            decrypted_text[col] += symbol  # Thêm ký tự vào cột hiện tại
+            col += 1
+
+            # Khi đạt tới cuối cột, chuyển sang dòng tiếp theo
+            if col == key or (col == key - 1 and row >= len(text) % key):
+                col = 0
+                row += 1
+
+        return ''.join(decrypted_text)     # Ghép các ký tự lại thành chuỗi kết quả
